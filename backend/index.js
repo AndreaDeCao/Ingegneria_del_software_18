0d5eccb0e26d9c006c1491ca827a11ff68448041
OmarBalavac <omarbalavac3@gmail.com>
2026-05-01 19:37:51 +0200

    MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,20 @@
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+
 const express = require("express");
 const cors = require("cors");
+
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+// Connessione a MongoDB
+mongoose.connect(process.env.MONGODB_URI, { family: 4 })
+  .then(() => console.log("MongoDB connesso!"))
+  .catch(err => console.error("Errore connessione:", err));
 
 // dati finti
 const treks = [
@@ -68,6 +79,21 @@ app.get("/treks", (req, res) => {
   res.json(treks);
 });
 
+/**
+ * Restituisce un singolo percorso in base all'id.
+ * @route GET /treks/:id
+ * @param {number} req.params.id - L'id del percorso
+ * @returns {Object} Il percorso trovato
+ * @returns {Object} 404 - Percorso non trovato
+ */
+
+app.get("/treks/:id", (req, res) => {
+  const trek = treks.find(t => t.id === parseInt(req.params.id));
+  if (!trek) return res.status(404).json({ error: "Percorso non trovato" });
+  res.json(trek);
+});
+
 app.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
 });
+
