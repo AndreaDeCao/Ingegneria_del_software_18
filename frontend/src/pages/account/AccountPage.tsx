5fabdb0b41869a887e5b0da67d66cc06a2736c9e
Andrea <decao.andrea.04@gmail.com>
2026-05-09 08:25:45 +0200

    aggiunta feature dropdown menu

--- frontend/src/pages/account/AccountPage.tsx
+++ frontend/src/pages/account/AccountPage.tsx
@@ -2,5 +2,5 @@ import { useParams } from "react-router-dom";
 
 export default function AccountPage() {
   const { sezione } = useParams();
-  return <h1>Account - {sezione}</h1>;
+  return <main><h2>Account — {sezione}</h2></main>;
 }
