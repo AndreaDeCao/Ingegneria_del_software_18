1523caa416f88d5f2b771eb9c2d5de6e4c053b46
OmarBalavac <omarbalavac3@gmail.com>
2026-05-25 21:28:58 +0200

    feat: aggiunto nei models friendships e aggiornato activities

--- backend/models/activities.js
+++ backend/models/activities.js
@@ -5,14 +5,33 @@ const ActivitySchema = new mongoose.Schema({
     title: { type: String, required: true },
     description: String,
     activityDate: { type: Date, required: true}, // Data e ora dell'attività. 
-    maxParticipants: Number,
+    maxParticipants: { type: Number, default: 6, max: 6},
+
     status: {
         type: String,
-        enum: ["Aperto", "Chiuso", "Annullato"]
+        enum: ["Aperto", "Chiuso", "Annullato"],
+        default: "Aperto",
     },
 
+    // Visibilità attività (public o private)
+    visibility: {
+        type: String,
+        enum: ["public", "private"],
+        default: "public",
+    },
+    
+    // Utetìnte che crea attività
     organizerID: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    trekID: { type: mongoose.Schema.Types.ObjectId, ref: 'Trek' }
-});
+
+    // ID Percorso dell'attività
+    trekID: { type: mongoose.Schema.Types.ObjectId, ref: 'Trek' },
+
+    // Lista utenti partecipanti all'attività (organizzatore compreso)
+    participants: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+
+    // Lista utenti invitati all'attività (può avere solo amici dell'organizzatore)
+    invitedUsers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+},
+{ timestamps: true });
 
 module.exports = mongoose.model("Activity", ActivitySchema);
