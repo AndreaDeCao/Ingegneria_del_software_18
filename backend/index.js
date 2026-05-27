85106afdef75371245badb5a9025f4aa6447b242
OmarBalavac <omarbalavac3@gmail.com>
2026-05-27 18:47:49 +0200

    feat: aggiunge route amicizie in index.js

--- backend/index.js
+++ backend/index.js
@@ -53,6 +53,7 @@ const authRoutes = require("./routes/authRoutes");
 const activityRoutes = require("./routes/activityRoutes"); //!!!
 const diaryRoutes = require("./routes/diaryRoutes");
 const routeRoutes = require("./routes/routeRoutes");
+const friendshipRoutes = require("./routes/friendshipRoutes");
 
 app.use("/treks", trekRoutes);
 app.use("/users", userRoutes);
@@ -61,6 +62,7 @@ app.use("/activities", activityRoutes);
 app.use("/api/weather", weatherRoutes); 
 app.use("/api/diary", diaryRoutes);
 app.use("/api/route", routeRoutes);
+app.use("/api/friendships", friendshipRoutes);
 
 // Connessione a MongoDB
 mongoose.connect(process.env.MONGODB_URI, { family: 4 })  // Imposta family: 4 per forzare l'uso di IPv4
@@ -81,6 +83,7 @@ mongoose.connect(process.env.MONGODB_URI, { family: 4 })  // Imposta family: 4 p
 //   if (!trek) return res.status(404).json({ error: "Percorso non trovato" });
 //   res.json(trek);
 // });
+
 
 app.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
