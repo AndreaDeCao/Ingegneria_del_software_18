a42d319c9969a469f39fbc98d17b49120d16cd89
OmarBalavac <omarbalavac3@gmail.com>
2026-05-27 18:38:21 +0200

    feat: aggiornate route attività

--- backend/routes/activityRoutes.js
+++ backend/routes/activityRoutes.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const router = express.Router();
 const activityController = require("../controllers/activityController");
+const requireAuth = require("../middleware/requireAuth");
 
 //const Activity = require("../models/Activity");
 
 
 router.get("/", activityController.getActivities);
 
-router.post("/", activityController.createActivity);
+router.get("/mine", requireAuth, activityController.getMyActivities);
+router.get("/joined", requireAuth, activityController.getJoinedActivities);
+router.get("/invited", requireAuth, activityController.getInvitedActivities);
+router.get("/:id", activityController.getActivityById);
+
+router.post("/",requireAuth, activityController.createActivity);
+
+router.put("/:id/join", requireAuth, activityController.joinActivity);
+router.put("/:id/leave", requireAuth, activityController.leaveActivity);
+
+router.patch("/:id/cancel", requireAuth, activityController.cancelActivity);
+router.patch("/:id/uncancel", requireAuth, activityController.uncancelActivity);
+router.patch("/:id/close", requireAuth, activityController.closeActivity);
+router.patch("/:id/open", requireAuth, activityController.openActivity);
+
+router.delete("/:id", requireAuth, activityController.deleteActivity);
 
 module.exports = router;
