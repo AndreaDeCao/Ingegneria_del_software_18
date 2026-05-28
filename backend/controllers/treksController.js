cf2662505795b3c9323b0c1f5d76cbb9887863b4
mayaaaa03 <145570299+mayaaaa03@users.noreply.github.com>
2026-05-28 17:49:20 +0200

    minor fix in trekController (getTrekById funziona sia con id che con _id
    modifica css

--- backend/controllers/treksController.js
+++ backend/controllers/treksController.js
@@ -1,3 +1,4 @@
+const mongoose = require("mongoose");
 const Trek = require("../models/treks");
 
 // GET tutti i percorsi
@@ -21,20 +22,27 @@ exports.createTrek = async (req, res) => {
   }
 };
 
-/**
- * Restituisce un percorso in base all'id
- * @param {string} id - id percorso
- * @returns {object} percorso restituito
- */
-
+// GET per ID (mongo o numerico)
 exports.getTreksById = async (req, res) => {
   try {
-    const trek = await Trek.findById(req.params.id);
-    if(!trek){
-      return res.status(404).json({error: "Percorso non trovato"});
+    const { id } = req.params;
+
+    let trek;
+
+    if (mongoose.Types.ObjectId.isValid(id)) {
+      trek = await Trek.findById(id);
+    } 
+    else if (!isNaN(parseInt(id))) {
+      trek = await Trek.findOne({ id: parseInt(id) });
     }
+
+    if (!trek) {
+      return res.status(404).json({ error: "Percorso non trovato" });
+    }
+
     res.json(trek);
-  } catch(err) {
+
+  } catch (err) {
     res.status(500).json({ error: err.message });
   }
-}
+};
