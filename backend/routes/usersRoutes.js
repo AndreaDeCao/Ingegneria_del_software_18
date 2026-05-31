b92c9a23439c10630c804cc541681196d5d84544
Andrea <decao.andrea.04@gmail.com>
2026-05-31 16:04:11 +0200

    minor fix post merge

--- backend/routes/usersRoutes.js
+++ backend/routes/usersRoutes.js
@@ -4,6 +4,15 @@ const userController = require("../controllers/usersController");
 const requireAuth = require("../middleware/requireAuth");
 
 
+router.get("/me", requireAuth, userController.getMe);
+router.put("/me", requireAuth, userController.updateMe);
+router.delete("/me/avatar", requireAuth, userController.deleteAvatar);
+router.delete("/me", requireAuth, userController.deleteMe);
+
+router.put("/me/avatar", requireAuth, userController.updateAvatar);
+
+router.put("/me/password", requireAuth, userController.updatePassword);
+
 router.get("/", userController.getUsers);
 router.get("/favorites", requireAuth, userController.getFavoriteTreks);
 router.get("/:id", userController.getUserById);
@@ -11,15 +20,8 @@ router.get("/:id", userController.getUserById);
 router.post("/", userController.createUser);
 router.post("/favorites/:trekId", requireAuth, userController.addTrekToFavorites);
 
-router.get("/me", requireAuth, userController.getMe);
-router.put("/me", requireAuth, userController.updateMe);
-router.delete("/me/avatar", requireAuth, userController.deleteAvatar);
-router.delete("/me", requireAuth, userController.deleteMe);
+
 router.get("/verify-email-change/:token", userController.verifyEmailChange);
-router.put("/me/avatar", requireAuth, userController.updateAvatar);
-
-router.put("/me/password", requireAuth, userController.updatePassword);
-
 
 
 module.exports = router;
